c8f54992ac558559e72de469faef15082425d72d
klmwook <klmwook72@gmail.com>
2023-09-08 12:58:35 +0900

    ssr방식으로 영화데이터 props로 전달

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,16 +3,25 @@ import logo from '@/public/img/logo.svg';
 import profile from '@/public/img/profile.png';
 import { FaBell, FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 function Header() {
+	const [Scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => (window.scrollY > 0 ? setScrolled(true) : setScrolled(false));
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<header>
+		<header className={`transition-color duration-[.5s] ${Scrolled && 'bg-[#141414]'}`}>
 			<div className='flex items-center space-x-2 md:space-x-10'>
 				<h1>
 					<Image src={logo} alt='netflex' width={100} height={100} className='cursor-pointer' />
 				</h1>
 
-				<ul className='space-x-4 md:flex'>
+				<ul className='space-x-4 hidden md:flex'>
 					<li className='headerLink'>HOME</li>
 					<li className='headerLink'>TV Show</li>
 					<li className='headerLink'>Movies</li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,31 @@
 import Header from '@/components/Header';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import requests from '@/utils/request';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ sf }) => {
+	console.log(sf);
 	return (
-		<div>
+		<div className='relatvie h-screen bg-gradient-to-b from-[#333] to=[#141414]'>
 			<Head>
-				<title>NETFLEX - 사이트 개발 공부</title>
+				<title>NEXTFLIX</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<Header></Header>
-			<main>
-				<h1>NETFLEX - 연습</h1>
-			</main>
+
+			<Header />
+
+			<main></main>
 		</div>
 	);
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const sf = await fetch(requests.sf).then((res) => res.json());
+	console.log(sf);
+
+	return {
+		props: { sf: sf.results },
+	};
+};
