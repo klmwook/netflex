2ca66964e147f3938a74dc909384de8f2c7c5105
klmwook <klmwook72@gmail.com>
2023-09-08 13:14:58 +0900

    여러개의 장르 API 데이터 추가 Promise.all 사용

--- utils/request.ts
+++ utils/request.ts
@@ -1,8 +1,13 @@
-const API_KEY = 'b18e798ff377ef49f1c335283e7c43d6';
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const requests = {
-	sf: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genred=878`,
+	top: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&languages=en-US`,
+	sf: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=878`,
+	drama: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=18`,
+	comedy: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=14`,
+	fantasy: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=35`,
+	action: `${BASE_URL}/discover/movie?api_key=${API_KEY}&languages=en-US&with_genres=28`,
 };
 
 export default requests;
