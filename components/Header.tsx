8c8a4ee0f0160c3805d1556d008cc99d1cb15812
klmwook <klmwook72@gmail.com>
2023-09-08 11:02:25 +0900

    기본 메인 페이지에 header 연결 및 타입 지정

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+	return (
+		<header>
+			<h1>NETFLEX - 연습</h1>
+
+			<ul>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
