b52636916e5cc21ffc463cd418bbaa309e5ba856
klmwook <klmwook72@gmail.com>
2023-09-11 11:37:21 +0900

    텍스트 스타일링

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -16,10 +16,17 @@ function Banner({ original }: Props) {
 	}, [original]);
 
 	return (
-		<section>
-			<div>
-				<Image src={`${baseURL}${Movie?.backdrop_path}`} alt={`${Movie?.title || Movie?.name}`} fill priority />
+		<section className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
+			{/* pic frame */}
+			<div className='absolute top-0 left-0 z-[1] h-[95vh] w-full'>
+				<Image src={`${baseURL}${Movie?.backdrop_path}`} alt={`${Movie?.title || Movie?.name}`} fill priority quality={50} className='object-cover' />
 			</div>
+
+			{/* title */}
+			<h1 className='relative z-[3] text-2xl font-bold drop-shadow md:text-4xl lg:text-7xl'>{Movie?.title || Movie?.name}</h1>
+
+			{/* overview */}
+			<p className='relative z-[3] text-xs max-w-xs md:max-w-lg md:text-lg lg:max-w-2xl'>{Movie?.overview}</p>
 		</section>
 	);
 }
