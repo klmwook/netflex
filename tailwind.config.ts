e4e784506ae851271a23080a4bb65889d85fab31
klmwook <klmwook72@gmail.com>
2023-09-12 10:55:09 +0900

    tailwind.config에 animate 커스텀속성 등록

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,15 @@ const config: Config = {
 				gradient1:
 					'linear-gradient(to bottom, rgba(20,20,20,0), rgba(20,20,20,1))',
 			},
+			keyframes: {
+				rotation: {
+					'0%': { transform: 'rotate(0deg)' },
+					'100%': { transform: 'rotate(360deg)' },
+				},
+			},
+			animation: {
+				'ani-rotation': 'rotation 1s linear infinite',
+			},
 		},
 	},
 	plugins: [require('tailwind-scrollbar'), require('tailwind-scrollbar-hide')],
