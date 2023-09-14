1c079e716e907fac9aca31e5fc2ab7385fc398d9
klmwook <klmwook72@gmail.com>
2023-09-14 12:15:10 +0900

    loading클래스 tailwind 커스텀 클래스 등록

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -16,12 +16,12 @@ function Banner({ original }: Props) {
 	const loading = useRef<HTMLDivElement>(null);
 
 	return (
-		<section className='px-4 pb-20 pt-40  flex flex-col space-y-4 py-16 md:space-y-8 lg:space-y-12 lg:px-16 lg:h-[85vh] lg:justify-end overflow-hidden relative'>
+		<section className='h-[50vh] px-4 pb-20 pt-40 flex flex-col space-y-4 py-16 md:space-y-8 lg:space-y-12 lg:px-16 md:h-[60vh] lg:h-[85vh] lg:justify-end overflow-hidden relative'>
 			{InitialLoading.current ? (
 				//user인증정보 받아지기 전까지는 프레임 틀은 유지하되 로딩바만 보이고
 				//인증정보 받아지면 데이터 출력
 				//새로고침시 인증정보 바뀔때마다 두번 렌더링되는 것을 방지하기 위함
-				<div className='w-[40px] h-[40px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-4 border-t-[transparent] border-solid border-[orange] rounded-[50%] z-50 bg-red animate-rotation'></div>
+				<div className='loading'></div>
 			) : (
 				<>
 					{/* pic frame */}
@@ -36,10 +36,7 @@ function Banner({ original }: Props) {
 							onLoadingComplete={() => loading.current?.remove()}
 						/>
 						<div className='absolute bottom-0 left-0 w-full h-full bg-gradient1'></div>
-						<div
-							className='w-[40px] h-[40px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[50%] border-4 border-solid border-[orange] rounded-[50%] z-30 border-t-[transparent] bg-red animate-ani-rotation'
-							ref={loading}
-						></div>
+						<div ref={loading} className='loading'></div>
 					</div>
 
 					{/* title */}
