d6227cb317a72beda19e1f4925ade65034acf025
klmwook <klmwook72@gmail.com>
2023-09-14 12:23:20 +0900

    Recoil 기본 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
+import { AuthProvider } from '@/hooks/useAuth';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { AuthProvider } from '@/hooks/useAuth';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		//전역 context를 전달해주는 커스텀 컴포넌트로 전체 페이지 컴포넌트들을 wrapping
-		<AuthProvider>
-			<Component {...pageProps} />
-		</AuthProvider>
+		<RecoilRoot>
+			<AuthProvider>
+				<Component {...pageProps} />
+			</AuthProvider>
+		</RecoilRoot>
 	);
 }
