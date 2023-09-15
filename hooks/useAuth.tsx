3c26a5e84ced4adf39c4d640b3cb288415db76fa
klmwook <klmwook72@gmail.com>
2023-09-15 09:55:02 +0900

    소스 수정 및 해결방법 주석 정리

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -39,6 +39,16 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 	const [UserInfo, setUserInfo] = useState<User | null>(null);
 	const router = useRouter();
 
+	/* 
+		- 라우터 이동시 라우터가 너무 빨리 이동이 되서 abort fetch router 에러 문구 뜨는 경우
+		- 데이터가 fetching중이거나, 컴포넌트의 추가적인 데이터들이 fully loaded 않았을 때 라우터 변경으로 컴포넌트가 바뀌는 경우
+	
+		해결방안 (해당 코드는 1,2번으로 해결이 불가능 함 3번으로는 가능 - 전체 컴포넌트를 감싸고 있는 root 컴포넌트이기 때문에 unmount가 불가능하기 때문	)
+			1. router.push('/', undefined, {shallow:true})
+				- 라우트 이동시 fetching 기능 무시하고 router 이동만 처리 (실제 컴포넌트 변경 시)
+			2. 커스텀 훅을 생성하고 그 안쪽에서 router.onChangeComplete 이벤트를 활용해서 기존 라우터 변경이 끝난이후에 router.push 호출 (다른 hook이나 핸들러 함수 안쪽에서 호출 불가)			
+			3. useAuth 훅 안에서 유저정보값이 변경될 때 마다 setTimeout으로 강제 debouncing적용
+	*/
 	useEffect(() => {
 		console.log('auth');
 		onAuthStateChanged(auth, (user) => {
@@ -47,12 +57,13 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 				router.push('/');
 			} else {
 				setUserInfo(null);
-				router.push('/Login');
+				//메인에서 로그인 페이지 넘어갈때 0.6초 동안 push가 중복실행되지 못하도록 debouncing적용
+				setTimeout(() => router.push('/Login'), 600);
 			}
 			//한번이라도 인증로직이 실행되면 초기상태를 false로 변경
 			//user정보값이 받아지고 동기적으로 해당값을 false로 변경하기 위해서
 			//setTimeout을 이용해서 강제로 web api에 전달했다 받음 (동기화) : promise로 가능
-			InitialLoading.current = false;
+			setTimeout(() => (InitialLoading.current = false), 0);
 		});
 	}, []);
 
@@ -61,7 +72,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 		await createUserWithEmailAndPassword(auth, email, password)
 			.then((userInfo) => {
 				setUserInfo(userInfo.user);
-				router.push('/');
 			})
 			.catch((err) => alert(err.message));
 	};
@@ -71,7 +81,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 		await signInWithEmailAndPassword(auth, email, password)
 			.then((userInfo) => {
 				setUserInfo(userInfo.user);
-				router.push('/');
 			})
 			.catch((err) => alert(err.message));
 	};
@@ -89,7 +98,7 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 	const memoedContext = useMemo(() => ({ UserInfo, signIn, signUp, logout, InitialLoading }), [UserInfo, InitialLoading]);
 	//로그인정보값이 들어와있을때에만 화면 출력
 	//실시간으로 적용되는 IntialLoading값을 전역 context에 담음
-	return <AuthContext.Provider value={memoedContext}>{children}</AuthContext.Provider>;
+	return <AuthContext.Provider value={memoedContext}>{!InitialLoading.current ? children : <div className='loading'></div>}</AuthContext.Provider>;
 };
 
 export default function useAuth() {
--- recoil/globalAtom.ts
+++ recoil/globalAtom.ts
@@ -2,11 +2,13 @@ import { atom } from 'recoil';
 import { Movie } from '@/types';
 
 export const modalState = atom<boolean>({
-	key: 'modalState',
+	//atom 객체의 key값이 고유값으로 인지되야되는데 되지 않았음.
+	//next에서 pre-render가 미리 되기 때문에 서버에서 한번, hydrate때 또 한번 컴포넌트가 구동 될 때 마다 고유값을 뒤에 추가로 'performance.now()를 붙여서 고유값으로 지정하여 키값 중복 오류 해결
+	key: `modalState${performance.now()}`,
 	default: false,
 });
 
 export const movieState = atom<Movie | null>({
-	key: 'movieState',
+	key: `movieState${performance.now()}`,
 	default: null,
 });
